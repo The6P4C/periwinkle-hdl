b72f55cfdba35e74155a647f5ad1cc5eeb5c1052
the6p4c <me@doggirl.gay>
2019-12-07 22:07:17 +1000

    Add SIZ and SINZ SPR behaviour

--- cpu.v
+++ cpu.v
@@ -4,13 +4,17 @@ module cpu(
 	reg [39:0] progmem [0:255];
 	initial $readmemb("progmem.txt", progmem);
 
+	parameter SPR_PC = 0;
+	parameter SPR_SIZ = 8;
+	parameter SPR_SINZ = 9;
 	reg [31:0] reg_pc = 32'b0;
+
 	reg [31:0] reg_gprs [0:31];
 
 	/*
 	 * Instruction decoding
 	 */
-	reg [39:0] instr;
+	wire [39:0] instr = progmem[reg_pc];
 
 	wire instr_transfer = instr[39];
 	wire [31:0] instr_source = instr[37:6];
@@ -36,8 +40,10 @@ module cpu(
 			source_value = instr_source_literal;
 		end else begin
 			if (instr_source_reg_is_spr) begin
-				// TODO: Read SPR
-				source_value = 0;
+				case (instr_source_reg)
+					SPR_PC: source_value = reg_pc;
+					default: source_value = 0;
+				endcase
 			end else begin
 				source_value = reg_gprs[instr_source_reg];
 			end
@@ -47,19 +53,30 @@ module cpu(
 	/*
 	 * Destination writeback
 	 */
-	always @(posedge i_clk) begin
+	wire [31:0] next_pc_no_skip = reg_pc + 1;
+	wire [31:0] next_pc_skip = reg_pc + 2;
+
+	reg [31:0] next_pc;
+	always @(*) begin
+		next_pc = reg_pc + 1;
+
 		if (instr_dest_reg_is_spr) begin
-			// TODO: Write SPR
-		end else begin
-			reg_gprs[instr_dest_reg] <= source_value;
+			case (instr_dest_reg)
+				SPR_SIZ: next_pc = source_value == 32'b0
+					? next_pc_skip : next_pc_no_skip;
+				SPR_SINZ: next_pc = source_value != 32'b0
+					? next_pc_skip : next_pc_no_skip;
+			endcase
 		end
 	end
 
-	/*
-	 * Program counter processing
-	 */
 	always @(posedge i_clk) begin
-		instr <= progmem[reg_pc];
-		reg_pc <= reg_pc + 1;
+		if (instr_dest_reg_is_spr) begin
+			// TODO: Write to SPR
+		end else begin
+			reg_gprs[instr_dest_reg] <= source_value;
+		end
+
+		reg_pc <= next_pc;
 	end
 endmodule
